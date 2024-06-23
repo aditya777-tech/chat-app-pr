e2d494c7180e2aa83c2f9924fe8738abab668d41
aditya777-tech <139342712+aditya777-tech@users.noreply.github.com>
2024-06-23 17:08:39 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,19 @@ import connectToMongoDB from "./connectiontoMongo/ConnectToMongoDb.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-
-const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+const __dirname = path.resolve();
+
+
+
+app.use(express.json()); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
+// this middleware is used to serve static files:
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
