44edcc1bad4b65d98e70808093d32855d510e301
aditya777-tech <139342712+aditya777-tech@users.noreply.github.com>
2024-06-23 16:46:23 +0530

    socket part completed

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
-import cookieParser from 'cookie-parser';
-import connectToMongoDB from './connectiontoMongo/ConnectToMongoDb.js';
-import messageRoutes from './routes/message.routes.js'
-import userRoutes from './routes/user.routes.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./connectiontoMongo/ConnectToMongoDb.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const app = express();
+
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use(express.json()); // it is used to parse the json request for postman or from frontend:
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('/',(req,res)=>{
-    res.send('<h1>its working</h1>');
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-})
-
-app.use('/api/auth',authRoutes);
-app.use('/api/messages',messageRoutes);
-app.use('/api/users',userRoutes)
-
-app.listen(port,()=>{
-    connectToMongoDB();
-
-    console.log("server is listening at",port);
-})
-
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
